9726497917f9725cd8ab244a2d2f71fb4ec67aca
tanahiro2010 <herentongkegu087@gmail.com>
2026-06-26 10:21:45 +0900

    feat: Honoに環境変数・Cloudflare関連をバインド

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import { Env } from './types/env'
 import { Hono } from 'hono'
 import oauthRouter from './features/oauth'
 
-const app = new Hono()
+const app = new Hono<Env>()
   .route('oauth', oauthRouter)
 
 app.get('/', (c) => {
