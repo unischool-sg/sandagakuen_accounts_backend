6d9c6b39fc5a7a46ca21e53a96222f5321567ad5
tanahiro2010 <herentongkegu087@gmail.com>
2026-06-26 17:24:34 +0900

    refactor: 書き方を少々修正

--- src/features/oauth/callback/index.ts
+++ src/features/oauth/callback/index.ts
@@ -6,12 +6,14 @@ import { callbackOAuthScheme } from './scheme'
 
 const app = new Hono<Env>()
 
-app.get('/', () => zValidator('query', callbackOAuthScheme), async (c) => {
+app.get('/', zValidator('query', callbackOAuthScheme), async (c) => {
   const query = c.req.valid('query')
+  const db = c.get('db')
+  const client = c.get('googleClient')
   if (!query) return c.json({ error: true, message: "failed to parse request query" })
-  const { result, code } = CallbackService.show(query)
+  const [result, code] = await CallbackService.show(query, db, client)
 
-  return c.json(result, code || 200)
+  return c.json(result, code)
 })
 
 
