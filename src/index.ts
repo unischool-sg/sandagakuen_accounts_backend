1020b4a923b7e1cd46b41ac1b3541cec23c8e31f
tanahiro2010 <herentongkegu087@gmail.com>
2026-06-26 23:28:16 +0900

    feat: ミドルウェアを適応

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Env } from './types/env'
 import { Hono } from 'hono'
-import oauthRouter from './features/oauth'
+import { rootMiddleware } from './middlewares/root'
+import oauthRouter from './features/oauth/index'
 
 const app = new Hono<Env>()
+  .use(rootMiddleware)
   .route('oauth', oauthRouter)
 
 app.get('/', (c) => {
