b8394da96dc14322ad47e1b69b038434aae313b6
tanahiro2010 <herentongkegu087@gmail.com>
2026-06-28 18:55:20 +0900

    feat: 最低限のrepositoryを実装

--- src/features/oauth/callback/repository.ts
+++ src/features/oauth/callback/repository.ts
@@ -1,15 +1,21 @@
 import { DrizzleD1Database } from "drizzle-orm/d1";
-import { SQL } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { users } from "../../../db/scheme";
 
-type UserInsert = typeof users.$inferInsert;
-
+type UserInsert = typeof users.$inferInsert
+type UserSelect = typeof users.$inferSelect
+type UserWhere = {
+  [K in keyof UserSelect]?: UserSelect[K]
+}
 
 class CallbackRepository {
   constructor(private readonly db: DrizzleD1Database) { }
 
-  async findUser(where: SQL<unknown>) {
-    return this.db.select().from(users).where(where).execute()
+  async findUser(where: UserWhere) {
+    const conditions = Object.entries(where).map(
+      ([key, value]) => eq(users[key as keyof UserSelect], value as never)
+    )
+    return this.db.select().from(users).where(and(...conditions)).execute()
   }
 
   async insert(data: UserInsert) {
