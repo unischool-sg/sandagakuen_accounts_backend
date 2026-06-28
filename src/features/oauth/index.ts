f3c8c4b2f9b38fc7d776ea88550216b9e5bcc1c0
tanahiro2010 <herentongkegu087@gmail.com>
2026-06-28 20:58:46 +0900

    fix: インポート元を修正

--- src/features/oauth/index.ts
+++ src/features/oauth/index.ts
@@ -1,6 +1,6 @@
 import { Env } from '../../types/env'
 import { Hono } from 'hono'
-import callbackRouter from './callback/service'
+import callbackRouter from './callback/index'
 
 const app = new Hono<Env>()
   .route('/callback', callbackRouter)
