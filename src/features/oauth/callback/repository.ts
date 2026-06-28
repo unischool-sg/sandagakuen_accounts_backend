5e2ebe30e861f177842f96de6018b61a365e99f0
tanahiro2010 <herentongkegu087@gmail.com>
2026-06-28 12:07:50 +0900

    fix: whereのタイプ指定などを修正

--- src/features/oauth/callback/repository.ts
+++ src/features/oauth/callback/repository.ts
@@ -1,9 +1,21 @@
 import { DrizzleD1Database } from "drizzle-orm/d1";
+import { SQL } from "drizzle-orm";
+import { users } from "../../../db/scheme";
+
+type UserInsert = typeof users.$inferInsert;
+
 
 class CallbackRepository {
   constructor(private readonly db: DrizzleD1Database) { }
 
-  async find(where: Record<string, string>) {
-    return;
+  async findUser(where: SQL<unknown>) {
+    return this.db.select().from(users).where(where).execute()
+  }
+
+  async insert(data: UserInsert) {
+    return this.db.insert(users).values(data).execute()
   }
 }
+
+
+export { CallbackRepository }
