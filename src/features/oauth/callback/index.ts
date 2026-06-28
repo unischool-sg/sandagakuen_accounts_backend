3e348fbfc8a32e8042924168a84d5bedd24f3a5b
tanahiro2010 <herentongkegu087@gmail.com>
2026-06-28 12:06:54 +0900

    fix: client取得時のキー名を修正

--- src/features/oauth/callback/index.ts
+++ src/features/oauth/callback/index.ts
@@ -9,12 +9,11 @@ const app = new Hono<Env>()
 app.get('/', zValidator('query', callbackOAuthScheme), async (c) => {
   const query = c.req.valid('query')
   const db = c.get('db')
-  const client = c.get('googleClient')
+  const client = c.get('client')
   if (!query) return c.json({ error: true, message: "failed to parse request query" })
   const [result, code] = await CallbackService.show(query, db, client)
 
   return c.json(result, code)
 })
-
 
 export default app
