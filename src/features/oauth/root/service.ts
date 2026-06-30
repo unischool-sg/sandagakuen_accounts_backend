b6bf55221d2072d0f1186d16010adcaea8fb7c1e
tanahiro2010 <herentongkegu087@gmail.com>
2026-06-30 16:46:29 +0900

    fix: 環境変数の取得元及びOAuthのURL生成関数の引数を修正

--- src/features/oauth/root/service.ts
+++ src/features/oauth/root/service.ts
@@ -1,13 +1,12 @@
 import { Google } from "google-oauth-lib"
 
 class OAuthService {
-  static show(client: Google, baseUrl: string, state: string) {
+  static show(client: Google, baseUrl: string) {
     return client.oauth.url({
       response_type: "code",
       redirect_uri: baseUrl + "/oauth/callback",
       access_type: "offline",
       prompt: "consent",
-      state,
     })
   }
 }
